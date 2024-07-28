b2f07995e9b7515b0f1aba57cfdecbb1efd6f41c
Animesh  Singh <animeshrathore255@gmail.com>
2024-07-28 18:54:02 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@
     {
         title: "Fearless",
       singer: "Chris Linton",
-      audio: "/public/songs/fearless.mp3",
+      audio: "songs/fearless.mp3",
       cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQftCR87cSbZ5Au71O5KCL9-i66xpqzzzuftQ&s",
       id:1
     }
@@ -20,7 +20,7 @@
     {
       title: "Calamity",
     singer: "Zayn Malik",
-    audio: "/public/songs/calamity.mp3",
+    audio: "songs/calamity.mp3",
     cover: "https://images.genius.com/c89ced664734c7e46da3f827db2818f0.1000x1000x1.png",
     id:2
   },
@@ -28,7 +28,7 @@
   {
     title: "Wanna Be Yours",
   singer: "Artic Monkey",
-  audio: "/public/songs/wanna.mp3",
+  audio: "songs/wanna.mp3",
   cover: "https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163",
   id:3
 },
@@ -36,7 +36,7 @@
 {
   title: "See You Again",
 singer: "Charlie Puth",
-audio: "/public/songs/see.mp3",
+audio: "songs/see.mp3",
 cover: "https://www.billboard.com/wp-content/uploads/media/wiz-khalifa-see-you-again-vid-billboard-1548.jpg",
 id:4
 },
@@ -46,7 +46,7 @@ id:4
 {
   title: "Sweat",
 singer: "Zayn Malik",
-audio: "/public/songs/sweat.mp3",
+audio: "songs/sweat.mp3",
 cover: "https://images.genius.com/c89ced664734c7e46da3f827db2818f0.1000x1000x1.png",
 id:5
 }
