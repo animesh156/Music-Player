8f035c6520c253fecf98334d4d19c95270c5c43e
animesh <animeshrathore255@gmail.com>
2025-01-21 01:13:54 +0530

    added new song & updated song url

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@
     {
       title: "Him & I",
       singer: "G Eazy",
-      audio:"songs/Him.mp3",
+      audio:"https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401948/G-Eazy_-_Him_I_dqdhqf.mp3",
       cover: "https://i.scdn.co/image/ab67616d0000b273046527a9c176f7c2916f3530",
       id:0
     },
@@ -12,7 +12,7 @@
     {
         title: "Fearless",
       singer: "Chris Linton",
-      audio: "songs/fearless.mp3",
+      audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401199/Fearless_PagalWorld_rdelnt.mp3",
       cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQftCR87cSbZ5Au71O5KCL9-i66xpqzzzuftQ&s",
       id:1
     }
@@ -20,7 +20,7 @@
     {
       title: "Calamity",
     singer: "Zayn Malik",
-    audio: "songs/calamity.mp3",
+    audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401100/Calamity_PagalWorld.com.se_pu7qfo.mp3",
     cover: "https://images.genius.com/c89ced664734c7e46da3f827db2818f0.1000x1000x1.png",
     id:2
   },
@@ -28,16 +28,16 @@
   {
     title: "Wanna Be Yours",
   singer: "Artic Monkey",
-  audio: "songs/wanna.mp3",
+  audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401240/I-Wanna-Be-Yours_PagalWorld_fzcdau.mp3",
   cover: "https://i.scdn.co/image/ab67616d0000b2734ae1c4c5c45aabe565499163",
   id:3
 },
 
 {
-  title: "See You Again",
-singer: "Charlie Puth",
-audio: "songs/see.mp3",
-cover: "https://www.billboard.com/wp-content/uploads/media/wiz-khalifa-see-you-again-vid-billboard-1548.jpg",
+  title: "Rasputin",
+singer: "Boney M",
+audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401935/Boney-M.-Rasputin-Official-Audio_lljcnj.mp3",
+cover: "https://res.cloudinary.com/de0jzo3mv/image/upload/v1737401756/boneym_njw2pe.jpg",
 id:4
 },
 
@@ -46,7 +46,7 @@ id:4
 {
   title: "Sweat",
 singer: "Zayn Malik",
-audio: "songs/sweat.mp3",
+audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401561/Sweat_PagalWorld.com.se_m9jdac.mp3",
 cover: "https://images.genius.com/c89ced664734c7e46da3f827db2818f0.1000x1000x1.png",
 id:5
 }
@@ -54,10 +54,23 @@ id:5
 {
   title: "Cheap Thrills",
 singer: "Sia",
-audio: "songs/chip.mp3",
+audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737401274/Sia-Cheap-Thrills_koeq22.mp3",
 cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY0nPxFt6pc__7udTQI2I63ROaZtGSPtvieg&s",
 id:6
+},
+
+
+{
+  title: "Fire Inside",
+singer: "Zayn",
+audio: "https://res.cloudinary.com/de0jzo3mv/video/upload/v1737400787/jlqypfuanfltsa1mhkb7.mp3",
+cover: "https://res.cloudinary.com/de0jzo3mv/image/upload/v1737401003/ovxyczhtjxtmev8ctpvd.jpg",
+id:7
 }
+
+
+
+
 
 ];
 
